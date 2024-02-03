31d7765c807de8f8869241d17a01cce9e79bd90e
Rajashekarksreddy <rajksreddy@gmail.com>
2024-02-03 11:03:48 +0530

    still-iterating

--- app/page.js
+++ app/page.js
@@ -1,14 +1,16 @@
 import ContactForm from "@/components/ContactForm";
 import Section from "@/components/section";
+import Features from "@/components/features";
 
 
 import SiteHeader from "@/components/siteheader";
 
 export default function Home() {
   return (
-    <div>
+    <div className=" bg-white">
         <SiteHeader/>
         <Section/>
+        <Features/>
         <ContactForm/>
     </div>
   );
--- components/section.jsx
+++ components/section.jsx
@@ -1,8 +1,10 @@
 
 export default function Section(){
     return(
-        <div className=" py-9 w-full flex text-center ">
-            <h1 className=" m-auto px-8">AT KESSAKU, THE WORLD REVOLVES AROUND YOU.</h1>
+        <div className=" py-9 w-full flex flex-col text-center ">
+            <h1 className=" text-4xl px-12 text-center">AT KESSAKU, THE WORLD REVOLVES AROUND YOU.</h1>
+
+            <p className=" py-6 px-4">The Phoenix Mills Ltd, is over a 100 year old company with over 17.5 mn sq ft Hospitality, Commercial and Residential assets spread over 100+ acres. This residential property offers a total of 180 units with various configurations such as 3,4,5,6,7,8,9 bhk. The status of this project is Pre Launch and is spread across an area of 4Acres. Phoenix Kessaku has apartments that are available on sale and are priced between Rs.4.75 Cr.</p>
         </div>
     )
 }
