89cbfa4796c11ef6ebf3be6b3f561ac4bfdf97e1
Rajashekarksreddy <rajksreddy@gmail.com>
2024-02-04 12:38:25 +0530

    hmm

--- components/Disclaimer.jsx
+++ components/Disclaimer.jsx
@@ -7,6 +7,8 @@ export default function Disclaimer(){
             <p>
      
         RERA Numbers: SORA of Kessaku - PRM/KA/RERA/1251/309/PR/171015/000416, NIWA of Kessaku - PRM/KA/RERA/1251/309/PR/171015/000431, MIZU of Kessaku - PRM/KA/RERA/1251/309/PR/171019/000441, FAIA of Kessaku - PRM/KA/RERA/1251/309/PR/171015/000454, ZEFA of Kessaku - PRM/KA/RERA/1251/309/PR/171019/000449</p>
+
+        <p className=" py-2">This site is for information purpose only and should not be treated as the official information.</p>
         </div>
     )
 }
