046ca42a3c3c0a71945a261113e532bcdcf23df1
Rajashekarksreddy <rajksreddy@gmail.com>
2024-02-04 13:05:52 +0530

    ship

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,10 @@
 export default function Dashboard(){
  return(
-    <div className=" flex flex-col justify-center text-center bg-blue-500">
+    <main className=" bg-blue-400">
+    <div className=" flex flex-col justify-center text-center">
         <h1 className=" text-white"> thank you for your interest</h1>
         <p className=" text-white">  our representative will get in touch with you shortly.</p>
     </div>
+    </main>
  ) 
 }
